82fc3e87db4ed753136d3ea1d679b60727d0a084
YBDmatS <sasia.mathilde@gmail.com>
2026-04-29 14:42:02 +0200

    [REFACTOR] replace inline authorization with @PreAuthorize in UserController

--- back/src/main/java/com/openclassrooms/starterjwt/controllers/UserController.java
+++ back/src/main/java/com/openclassrooms/starterjwt/controllers/UserController.java
@@ -3,30 +3,21 @@ package com.openclassrooms.starterjwt.controllers;
 import com.openclassrooms.starterjwt.mapper.UserMapper;
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.services.UserService;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
     private final UserMapper userMapper;
     private final UserService userService;
-
-
-    public UserController(UserService userService,
-                          UserMapper userMapper) {
-        this.userMapper = userMapper;
-        this.userService = userService;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable("id") String id) {
@@ -44,24 +35,15 @@ public class UserController {
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> save(@PathVariable("id") String id) {
-        try {
-            User user = this.userService.findById(Long.valueOf(id));
+    @PreAuthorize("@userSecurity.isOwner(#id, authentication.name)")
+    public ResponseEntity<?> save(@PathVariable("id") Long id) {
+        User user = this.userService.findById(id);
 
-            if (user == null) {
-                return ResponseEntity.notFound().build();
-            }
-
-            UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-            if (!Objects.equals(userDetails.getUsername(), user.getEmail())) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            }
-
-            this.userService.delete(Long.parseLong(id));
-            return ResponseEntity.ok().build();
-        } catch (NumberFormatException e) {
-            return ResponseEntity.badRequest().build();
+        if (user == null) {
+            return ResponseEntity.notFound().build();
         }
+
+        this.userService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }

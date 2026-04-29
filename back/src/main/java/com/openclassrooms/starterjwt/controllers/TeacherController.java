1c661bcdca0f9c7bba777df70aca271273a62020
YBDmatS <sasia.mathilde@gmail.com>
2026-04-29 16:21:07 +0200

    [REFACTOR] remove try/catch NumberFormatException from controllers

--- back/src/main/java/com/openclassrooms/starterjwt/controllers/TeacherController.java
+++ back/src/main/java/com/openclassrooms/starterjwt/controllers/TeacherController.java
@@ -22,17 +22,13 @@ public class TeacherController {
 
     @GetMapping("/{id}")
     public ResponseEntity<TeacherDto> findById(@PathVariable("id") String id) {
-        try {
-            Teacher teacher = this.teacherService.findById(Long.valueOf(id));
+        Teacher teacher = this.teacherService.findById(Long.valueOf(id));
 
-            if (teacher == null) {
-                return ResponseEntity.notFound().build();
-            }
-
-            return ResponseEntity.ok().body(this.teacherMapper.toDto(teacher));
-        } catch (NumberFormatException e) {
-            return ResponseEntity.badRequest().build();
+        if (teacher == null) {
+            return ResponseEntity.notFound().build();
         }
+
+        return ResponseEntity.ok().body(this.teacherMapper.toDto(teacher));
     }
 
     @GetMapping()

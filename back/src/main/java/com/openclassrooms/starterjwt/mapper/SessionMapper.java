dfd90f01510de28f9b43e0ff2774f35db2a7fad0
YBDmatS <sasia.mathilde@gmail.com>
2026-04-30 12:36:21 +0200

    [REFACTOR] remove redundant description mapping in SessionMapper

--- back/src/main/java/com/openclassrooms/starterjwt/mapper/SessionMapper.java
+++ back/src/main/java/com/openclassrooms/starterjwt/mapper/SessionMapper.java
@@ -8,12 +8,10 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = {SessionMappingHelper.class})
 public interface SessionMapper extends EntityMapper<SessionDto, Session> {
 
-    @Mapping(source = "description", target = "description")
     @Mapping(source = "teacherId", target = "teacher", qualifiedByName = "idToTeacher")
     @Mapping(source = "users", target = "users", qualifiedByName = "userIdsToUsers")
     Session toEntity(SessionDto sessionDto);
 
-    @Mapping(source = "description", target = "description")
     @Mapping(source = "teacher.id", target = "teacherId")
     @Mapping(source = "users", target = "users", qualifiedByName = "usersToUserIds")
     SessionDto toDto(Session session);

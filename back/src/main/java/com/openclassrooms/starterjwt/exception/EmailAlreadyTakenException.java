2abf980e5a57dbe6473e7290176a0f73817b58b2
YBDmatS <sasia.mathilde@gmail.com>
2026-04-30 14:54:28 +0200

    [REFACTOR] apply @Serial annotation for serialVersionUID field across all classes

--- back/src/main/java/com/openclassrooms/starterjwt/exception/EmailAlreadyTakenException.java
+++ back/src/main/java/com/openclassrooms/starterjwt/exception/EmailAlreadyTakenException.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.starterjwt.exception;
 
+import java.io.Serial;
+
 public class EmailAlreadyTakenException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 }

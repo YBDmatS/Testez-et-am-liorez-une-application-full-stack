a2e9e66b35b6fec81ccf9f7905dbd929183c85f9
YBDmatS <sasia.mathilde@gmail.com>
2026-04-30 12:34:37 +0200

    [REFACTOR] remove dead null check in UserSecurity

--- back/src/main/java/com/openclassrooms/starterjwt/security/UserSecurity.java
+++ back/src/main/java/com/openclassrooms/starterjwt/security/UserSecurity.java
@@ -1,6 +1,5 @@
 package com.openclassrooms.starterjwt.security;
 
-import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,7 +10,6 @@ public class UserSecurity {
     private final UserService userService;
 
     public boolean isOwner(Long id, String authenticatedEmail) {
-        User user = userService.findById(id);
-        return user != null && user.getEmail().equals(authenticatedEmail);
+        return userService.findById(id).getEmail().equals(authenticatedEmail);
     }
 }

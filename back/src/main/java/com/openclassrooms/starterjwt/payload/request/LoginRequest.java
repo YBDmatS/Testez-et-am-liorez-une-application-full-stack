34be0d75eb0d1a3932112d6227b580ffb28f0f86
YBDmatS <sasia.mathilde@gmail.com>
2026-04-30 11:07:56 +0200

    [REFACTOR] clean up DTO and payload validation annotations

--- back/src/main/java/com/openclassrooms/starterjwt/payload/request/LoginRequest.java
+++ back/src/main/java/com/openclassrooms/starterjwt/payload/request/LoginRequest.java
@@ -1,12 +1,16 @@
 package com.openclassrooms.starterjwt.payload.request;
 
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
     @NotBlank
+    @Size(max = 50)
+    @Email
     private String email;
 
     @NotBlank

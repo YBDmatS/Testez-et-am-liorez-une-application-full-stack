b52abb51be107e00480584715705f2a063c242b2
YBDmatS <sasia.mathilde@gmail.com>
2026-04-30 12:39:35 +0200

    [REFACTOR] add serialVersionUID to custom exceptions

--- back/src/main/java/com/openclassrooms/starterjwt/exception/NotParticipatingException.java
+++ back/src/main/java/com/openclassrooms/starterjwt/exception/NotParticipatingException.java
@@ -1,4 +1,5 @@
 package com.openclassrooms.starterjwt.exception;
 
 public class NotParticipatingException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 }
--- back/src/main/java/com/openclassrooms/starterjwt/exception/ResourceNotFoundException.java
+++ back/src/main/java/com/openclassrooms/starterjwt/exception/ResourceNotFoundException.java
@@ -1,4 +1,5 @@
 package com.openclassrooms.starterjwt.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 }
